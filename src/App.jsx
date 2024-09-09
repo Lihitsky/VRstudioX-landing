524cbe985e49c4184c3caa58e9db6e5e21b1aedf
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-09-09 19:53:48 +0300

    feat(app): integrate HeroSection, FeatureSection, and WorkflowSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { NavBar } from "./components/nav-bar";
+import { HeroSection } from "./components/hero-section";
+import { FeatureSection } from "./components/feature-section";
+import { WorkflowSection } from "./components/workflow-section";
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <WorkflowSection />
+      </div>
     </>
   );
 };
