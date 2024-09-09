1e58e7b800ce68bb2f2bbb1516ff0a1162f746da
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-09-09 19:51:41 +0300

    feat(components): add HeroSection with videos and call-to-action buttons

---
+++ src/components/hero-section/index.jsx
@@ -0,0 +1,52 @@
+import video1 from "../../assets/video1.mp4";
+import video2 from "../../assets/video2.mp4";
+
+export const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VR studio X build tools
+        <span className="bg-gradient-to-r from-[#FAD36F] to-[#E0FF1F] text-transparent bg-clip-text">
+          {" "}
+          for developers
+        </span>
+      </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4x">
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
+      </p>
+      <div className="flex justify-center my-10">
+        <a
+          href="#"
+          className="bg-gradient-to-r from-[#FAD36F] to-[#E0FF1F] py-3 px-4 mx-3 rounded-md text-black"
+        >
+          Start for free
+        </a>
+        <a href="#" className="py-3 px-3 mx-2 rounded-md border">
+          Documentation
+        </a>
+      </div>
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-[#FAD36F] shadow-[#E0FF1F] mx-2 my-4"
+        >
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-[#FAD36F] shadow-[#E0FF1F] mx-2 my-4"
+        >
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
